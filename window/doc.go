a2aaa555231b365e1e5c1e51a04e75166697b585
mlange-42 <martin_lange_@gmx.net>
2023-04-09 21:01:48 +0200

    extend package-level docs

--- window/doc.go
+++ window/doc.go
@@ -1,2 +1,4 @@
 // Package window provides an OpenGL window system for plotting and free drawing.
+//
+// For the actual visualization/plotting part, see package [github.com/mlange-42/arche-pixel/plot].
 package window
