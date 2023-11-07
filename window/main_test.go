f65b3d5ababdf21d68cab1152f69780b66e4c2ba
mlange-42 <martin_lange_@gmx.net>
2023-11-07 17:11:56 +0100

    add TestMain to run all tests on the main thread

---
+++ window/main_test.go
@@ -0,0 +1,14 @@
+package window_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gopxl/pixel/v2/backends/opengl"
+)
+
+func TestMain(m *testing.M) {
+	opengl.Run(func() {
+		os.Exit(m.Run())
+	})
+}
